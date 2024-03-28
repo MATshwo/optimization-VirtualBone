c4495e8d1ff4af401c9a41a4114ad0059f0150f9
wangwenbin <86559685+MATshwo@users.noreply.github.com>
2024-03-28 10:17:45 +0800

    Update Mytools.py

--- Mytools.py
+++ Mytools.py
@@ -15,40 +15,6 @@ import scipy
 from scipy.spatial.transform import Rotation
 import copy
 
-# region Defined-self about laplaican_smooth -- too slow    
-# def Smoothing_pos(edge_index,pos_x,normalization='rw'):
-#      """
-#      网格的Laplacian平滑,执行效率还是很低,pyg里貌似有可以直接执行的模块-GCN_norm?
-#      L_rw = I - D(-1)A
-#      L_sym = I - D(-0.5)A(-0.5)
-#      """
-#      # pos_x:[numvert,3]
-#      # 1.计算modified laplacian平滑矩阵L
-#      indexs,weights = get_laplacian(edge_index,normalization=normalization)
-#      print(indexs,weights)
-#      # 2.根据L对原始网格坐标进行平滑
-#      dpos = torch.zeros_like(pos_x)
-#      res = torch.mul(weights.reshape(-1,1),pos_x[indexs[1]])
-#      #for i in range(res.shape[1]):
-#      #    dpos[:,indexs[0,i]] = dpos[:,indexs[0,i]] + res[:,i]
-#      for i in range(pos_x.shape[1]):
-#          # 上一个循环遍历所有边,效率很低,修改为遍历顶点. [1,0,2,1]==[0,0,0,0]=[FTFF],然后sum(res[F,T,F,F])->dpos[0]
-#          # 经验证,两种方法返回结果一致
-#          tmpp = res[indexs[0]==(torch.zeros_like(indexs[0])+i)]
-#          dpos[i] = torch.sum(tmpp,dim=0)
-#      return dpos
-
-# def Laplacian_matrix(adj_matrix):
-#     """根据邻接矩阵(不需要self-loop)计算Laplacian变换矩阵
-#     PYG库中自带计算L矩阵的函数:get_laplacian"""
-#     # 1. 顶点规模:adj_matrix[1,num,num]
-#     num = adj_matrix.shape[1]
-#     # 2. L = (I-D^(-1)A)
-#     # 这里注意维度一定要对应:依赖输入matrix的shape
-#     L = torch.transpose(torch.sum(adj_matrix,dim=2).expand(1,num,num),1,2).to("cuda")
-#     L = torch.eye(num).reshape(1,num,num).to("cuda") - torch.div(adj_matrix,L)
-#     return L 
-
 
 def collision_detect(body_pose,body_trans,pred_pos):
     """基于fcl对执行布料和人体的碰撞检测&布料自碰撞检测
@@ -233,7 +199,6 @@ class reduction_tool_plus():
 
         elif self.method == "NMF":
             self.tool = NMF(n_components=self.n_componment,init="random",random_state=3439,solver="mu",max_iter=100) 
-            # input_data = np.exp(self.init_pos) # 将原始数据转换为非负
             self.min_temp = np.min(self.init_pos,axis=0)
             input_data = self.init_pos - self.min_temp
             self.tool.fit(input_data)
@@ -245,7 +210,7 @@ class reduction_tool_plus():
 
         batch_size = x.shape[0]
         x = torch.transpose(x,2,1).reshape(batch_size*3,-1)
-        x_ = x.detach().clone() # 从计算图剥离并拷贝
+        x_ = x.detach().clone() 
         if not is_fit:
             if self.method == "KPCA":                
                 x_ = self.tool._validate_data(x_.cpu().numpy(), accept_sparse="csr", reset=False)
@@ -254,7 +219,6 @@ class reduction_tool_plus():
                 scaled_alphas = np.zeros_like(self.tool.eigenvectors_)
                 scaled_alphas[:, non_zeros] = self.tool.eigenvectors_[:, non_zeros] / np.sqrt(self.tool.eigenvalues_[non_zeros])
                 x_reduce = torch.from_numpy(np.dot(K, scaled_alphas)).to(torch.float32).to(x.device)
-                #x_reduce = torch.from_numpy(self.tool.transform(x.cpu().numpy())).to(x.device).to(torch.float32) 
 
             elif self.method == "PCA":
                 x_reduce = torch.matmul(x - torch.from_numpy(self.tool.mean_).to(x.device), torch.from_numpy(self.tool.components_.T).to(x.device)).to(torch.float32) 
@@ -263,15 +227,14 @@ class reduction_tool_plus():
                 x_reduce = torch.matmul(x, torch.from_numpy(self.tool.components_.T).to(x.device).to(torch.float32))
             
             elif self.method == "dict":
-                x_reduce = torch.matmul(x, torch.from_numpy(self.tool.components_.T).to(x.device)).to(torch.float32) # shape:[3,3];[batchsize*3,reduce_dim]
-                mask = ~(x_reduce == (torch.max(x_reduce,dim = -2)[0].repeat(x_reduce.shape[0],1)))
-                #mask = not(x_reduce == (torch.max(x_reduce,dim = -2)[0].repeat(x_reduce.shape[0],1))) # 不能用not,会报错 -- not应该只能作用在单个逻辑变量..
-                x_reduce[mask] = 0  # 保留第一个维度的最大值,其余=0,按照源码transfrom的结果执行此操作 -- 原理暂不清楚
+                x_reduce = torch.matmul(x, torch.from_numpy(self.tool.components_.T).to(x.device)).to(torch.float32)
+                mask = ~(x_reduce == (torch.max(x_reduce,dim = -2)[0].repeat(x_reduce.shape[0],1))).
+                x_reduce[mask] = 0  
             
             elif self.method == "FA":        
                 Ih = np.eye(len(self.tool.components_))
                 x_transformed = x - torch.from_numpy(self.tool.mean_).to(x.device)
-                # FA中的Wpsi*Wpsi.T不等于单位阵,所以后续recover阶段需要使用pinv求解Wpsi.T矩阵的伪逆
+
                 self.tool.Wpsi = self.tool.components_ / self.tool.noise_variance_
                 self.tool.cov_z_inv = Ih + np.dot(self.tool.Wpsi, self.tool.components_.T)
                 cov_z = torch.from_numpy(scipy.linalg.inv(self.tool.cov_z_inv)).to(x.device)
@@ -281,14 +244,12 @@ class reduction_tool_plus():
             elif self.method == "NMF":
                 input_x = x_.cpu().numpy()
                 self.min_temp = np.min(input_x,axis=0)
-                # input_x = input_x - self.min_temp  # input_x - self.min_temp <==> x - torch.min(x,dim=0)
                 x_reduce = torch.matmul(x-torch.min(x,dim=0), torch.from_numpy(scipy.linalg.pinv(self.tool.components_)).to(torch.float32).to(x.device))
-                #x_reduce = torch.from_numpy(self.tool.transform(input_x.astype(np.float64))).to(torch.float32).to(x.device)
                 
         else:
             if self.method == "PCA":
                 self.tool = PCA(n_components=self.n_componment) 
-                self.tool.fit(x_.cpu().numpy()) # 刷新当前类的pca_tool
+                self.tool.fit(x_.cpu().numpy()) 
                 x_reduce = torch.matmul(x - torch.from_numpy(self.tool.mean_).to(x.device), torch.from_numpy(self.tool.components_.T).to(x.device)).to(torch.float32) 
 
                 
@@ -333,26 +294,17 @@ class reduction_tool_plus():
                 self.min_temp = np.min(input_x,axis=0)
                 input_x = input_x - self.min_temp
                 self.tool = NMF(n_components=self.n_componment,init="random",random_state=3439) 
-                self.tool.fit(input_x)  # 这里构造input_x主要是为了fit模型,只支持numpy数组类型
+                self.tool.fit(input_x)  
                 x_reduce = torch.matmul(x-torch.min(x,dim=0), torch.from_numpy(scipy.linalg.pinv(self.tool.components_)).to(torch.float32).to(x.device))
-                #x_reduce = torch.from_numpy(self.tool.transform(input_x.astype(np.float64))).to(torch.float32).to(x.device) 
                 
         return x_reduce.reshape(batch_size,1,-1,3)
      
     def recover_matrix(self,matrix):
-        # input: matrix:[reduce_dim,reduce_dim]
-        # output: recover_adj:[init_dim,init_dim]
-
 
         if self.method == "PCA":
             x_recover = torch.matmul(matrix.float(),torch.from_numpy(self.tool.components_).float().to(matrix.device)) # pca.components_ :[pca_dim,12273]pca转换矩阵
             x_recover = torch.matmul(torch.from_numpy(self.tool.components_.T),x_recover) + 0.5 * torch.from_numpy(self.tool.mean_).float().to(matrix.device)
             x_recover = x_recover.T + + 0.5*torch.from_numpy(self.tool.mean_).float().to(matrix.device)
-
-        
-        # elif self.method == "KPCA":
-        #     K = self.tool._get_kernel(matrix.cpu().numpy(), self.tool.X_transformed_fit_)
-        #     x_recover = 2.0 * torch.from_numpy(np.dot(K, self.tool.dual_coef_)).to(torch.float32).to(matrix.device).T
 
         elif self.method == "TSVD":
             x_recover = (torch.matmul(matrix.float(),torch.from_numpy(self.tool.components_).float().to(matrix.device))) #[12273,reduce_dim]
@@ -366,14 +318,12 @@ class reduction_tool_plus():
             tmp = torch.matmul(matrix.float(),torch.from_numpy(self.tool.cov_z_inv).float().to(matrix.device))  
             inv_w = torch.from_numpy(scipy.linalg.pinv(self.tool.Wpsi.T)).float().to(matrix.device)
             x_recover = torch.matmul(tmp,inv_w).T
-            #x_recover = (x_recover + 0.5 * torch.from_numpy(self.tool.mean_).float().to(matrix.device)).T
 
             x_recover = torch.matmul(x_recover,torch.from_numpy(self.tool.cov_z_inv).float().to(matrix.device))
             x_recover = torch.matmul(x_recover,inv_w) + 0.5 * torch.from_numpy(self.tool.mean_).float().to(matrix.device)
             x_recover = x_recover.T + 0.5 * torch.from_numpy(self.tool.mean_).float().to(matrix.device)
 
-        elif self.method == "NMF":
-            #x_recover = torch.log(torch.matmul(x.float(),torch.from_numpy(self.tool.components_).float().to(x.device))).T            
+        elif self.method == "NMF":        
             x_recover = torch.matmul(matrix.float(),torch.from_numpy(self.tool.components_).float().to(matrix.device)) 
             x_recover = torch.matmul(torch.from_numpy(self.tool.components_.T).float().to(matrix.device),x_recover) + 0.5 * torch.from_numpy(self.min_temp).float()
             x_recover = x_recover.T + 0.5 * torch.from_numpy(self.min_temp).float()
@@ -381,20 +331,16 @@ class reduction_tool_plus():
         return x_recover.reshape(self.numverts,self.numverts)
     
     def dim_recover(self,x):
-        # x:[batchsize,3*3]
    
         batch_size = x.shape[0]
         x = x.reshape(x.shape[0]*3,-1)
-        x_ = x.detach().clone() # 从计算图剥离并拷贝
+        x_ = x.detach().clone() 
 
         if self.method == "PCA":
-            x_recover = (torch.matmul(x.float(),torch.from_numpy(self.tool.components_).float().to(x.device)) + torch.from_numpy(self.tool.mean_).float().to(x.device)).T # pca.components_ :[pca_dim,12273]pca转换矩阵
-            #x_recover = self.tool.inverse_transform(x).T
+            x_recover = (torch.matmul(x.float(),torch.from_numpy(self.tool.components_).float().to(x.device)) + torch.from_numpy(self.tool.mean_).float().to(x.device)).T
         
         elif self.method == "KPCA":
-            
             K = self.tool._get_kernel(x_.cpu().numpy(), self.tool.X_transformed_fit_)
-
             x_recover = 2.0 * torch.from_numpy(np.dot(K, self.tool.dual_coef_)).to(torch.float32).to(x.device).T
 
         elif self.method == "TSVD":
@@ -408,42 +354,31 @@ class reduction_tool_plus():
             x_recover = torch.matmul(tmp,torch.from_numpy(scipy.linalg.pinv(self.tool.Wpsi.T)).to(torch.float32).to(x.device))
             x_recover = (x_recover + torch.from_numpy(self.tool.mean_).float().to(x.device)).T
             
-        elif self.method == "NMF":
-            #x_recover = torch.log(torch.matmul(x.float(),torch.from_numpy(self.tool.components_).float().to(x.device))).T            
+        elif self.method == "NMF":      
             x_recover = (torch.matmul(x.float(),torch.from_numpy(self.tool.components_).float().to(x.device)) + torch.from_numpy(self.min_temp).float()).T            
         return x_recover.reshape(batch_size,-1,3)
 
 class reduction_tool():
-    # 0414_reduction_plus1模型基于这个代码
-    # 对原始布料模型的顶点降维&还原 -- 针对顶点的降维
-    # 两种思路: 每个动作帧都执行一次PCA降维；2)只对初始模型进行PCA降维,后续降维直接使用初始的PCA变换矩阵计算
-
     def __init__(self,init_pos):
         self.method = "PCA"
-        self.init_pos = init_pos.T #[3,numverts] -> 对顶点降维
+        self.init_pos = init_pos.T
 
     def PCA_process(self):
-        # 首次PCA降维操作: 
         self.pca_tool = PCA(n_components=3) 
         self.pca_tool.fit(self.init_pos)
-        #new = self.pca_tool.fit_transform(self.init_pos)
         print("PCA_compoments:{},dim = {}.".format(np.sum(self.pca_tool.explained_variance_ratio_),3))
 
     def dim_reduction(self,x):
         
-        x = x.reshape(x.shape[0]*3,-1) #[batchsize*3,numverts]
+        x = x.reshape(x.shape[0]*3,-1) 
         x_reduce = torch.matmul(x - torch.from_numpy(self.pca_tool.mean_).to(x.device), torch.from_numpy(self.pca_tool.components_.T).to(x.device)).to(torch.float32) 
         return x_reduce.reshape(x.shape[0],1,-1,3)
     
     def dim_recover(self,x):
-        # x:[batchsize,3*3]
-        x_recover = torch.matmul(x.reshape(x.shape[0]*3,-1).float(),torch.from_numpy(self.pca_tool.components_).float().to(x.device)) + torch.from_numpy(self.pca_tool.mean_).float().to(x.device) # pca.components_ :[pca_dim,12273]pca转换矩阵  
-        # print(x_recover.shape)  # [15,12273]
+        x_recover = torch.matmul(x.reshape(x.shape[0]*3,-1).float(),torch.from_numpy(self.pca_tool.components_).float().to(x.device)) + torch.from_numpy(self.pca_tool.mean_).float().to(x.device) 
         return x_recover.reshape(x.shape[0],-1,3)
     
     def adj_trans(self,adj_matrix):
-        # 原始邻接矩阵降维
-        # 如果是3*3的话直接指定全连通即可
         adj_reduce = self.pca_tool.transform(self.pca_tool.transform(adj_matrix).T)
         return adj_reduce
 
@@ -453,29 +388,23 @@ class cluster_data(Dataset):
         self.pose = []
         self.trans = []
         self.cluster_id = cluster_id
-        self.length = length # 类内最大成员数
-        self.id_list = id_list  # 重排之后前i-1类成员的个数
+        self.length = length 
+        self.id_list = id_list 
         self.cluster_res = []
 
         for i in os.listdir(path)[:]:
-            # path指动作序列对应的path
+
             if "npz" in i :
                 tmp = np.load(os.path.join(path,i),allow_pickle=True)
                 if (tmp["pose"].shape != (500,52,3))or(tmp["trans"].shape != (500,3))or(tmp["sim_res"].shape!=(500,12273,3)) :
                     continue
                 final = torch.zeros(500,self.length,3)
-                #tmp2 = np.load(os.path.join("VirtualBoneDataset/dress02/affinity_cosine",i),allow_pickle=True)
                 tmp2 = np.load(os.path.join("VirtualBoneDataset/dress02/spectral_cosine",i),allow_pickle=True)
 
                 self.pose.append(torch.from_numpy(tmp["pose"]))
                 self.trans.append(torch.from_numpy(tmp["trans"]))
 
-                # 按聚类个数分批次的加载数据集: id表示第i个类别
-                # print("{}-th cluster loading...with {}.".format(cluster_id,self.id_list[cluster_id+1]-self.id_list[cluster_id]))
-                # 统一不同类的输入数据规模: 使用最大类内成员数作为统一维度,不足用0填充
-                
                 final[:,:(self.id_list[cluster_id+1]-self.id_list[cluster_id]),:] = torch.from_numpy(tmp2["spectral_res"][:,self.id_list[cluster_id]:self.id_list[cluster_id+1],:])
-                #final[:,:(self.id_list[cluster_id+1]-self.id_list[cluster_id]),:] = torch.from_numpy(tmp2["affinity_res"][:,self.id_list[cluster_id]:self.id_list[cluster_id+1],:])
                 self.cluster_res.append(final)
 
         self.len = len(self.pose)
@@ -483,7 +412,6 @@ class cluster_data(Dataset):
         for i in range(self.len):
             self.pose[i] = (self.pose[i] - state["pose_mean"]) / state["pose_std"]
             self.trans[i] = (self.trans[i] - self.trans[0] - state["trans_mean"]) / state["trans_std"]   
-        # print(self.len) # 80个有效数据: 测试集5;训练集75
 
     def __getitem__(self,idx):
 
@@ -526,39 +454,27 @@ class Mydata(Dataset):
                     self.trans.append(torch.from_numpy(tmp["trans"]))
                     self.sim_res.append(torch.from_numpy(tmp2["final_ground"]))
 
-        # 标准化处理
         self.len = len(self.pose)
         for i in range(self.len):
             self.pose[i] = (self.pose[i] - state["pose_mean"]) / state["pose_std"]
-            # self.trans[i] = (self.trans[i] - self.trans[0] - state["trans_mean"]) / state["trans_std"]   
             self.trans[i] = (self.trans[i] - self.trans[i][0] - state["trans_mean"]) / state["trans_std"]   
-        # print(self.len) # 80个有效数据: 测试集5;训练集75
 
     def __getitem__(self,idx):
         if self.mode == "HF": 
             return self.pose[idx],self.trans[idx],self.sim_res[idx]
         if self.mode == "LF": 
-            #return self.pose[idx],self.trans[idx],self.sim_res[idx]
             return self.pose[idx],self.trans[idx],self.sim_res[idx],self.dpos[idx]
-
+            
     def __len__(self):
         return self.len
 
-# def find_nearest(pred_pos):
-#     """
-#     输入预测布料顶点坐标
-#     返回与每个顶点距离最近的人体顶点坐标和法线 - 注意是人体坐标非虚拟骨骼坐标
-#     """
-#     body_pos = np.load() # 加载人体坐标数据
 
-#     return bpos,bnormal
 def get_full_edge_index(numverts=1,mode="empty"):
     # 根据顶点数量创建全孤立&全连通的邻接矩阵  -- 返回稀疏格式[2,edge_num]
     from torch_geometric.utils import dense_to_sparse    
     if mode == "empty":
-        adj = torch.zeros((numverts,numverts))  # 使用全连接导致最终输出无法区分不同维度的差别，所以由全连通改为全不连通
+        adj = torch.zeros((numverts,numverts))  
     elif mode == "full":
-        # 全连通邻接矩阵
         adj = torch.ones((numverts,numverts)) - torch.eye(numverts)
     return dense_to_sparse(adj)[0]
 
@@ -571,7 +487,6 @@ def Loss_func(pos,pred_pos,mode="L2",bool_col = 0):
     2. 绝对值损失
     3. 混合损失
     4. BCEloss: 需要将数据处理到[0,1]之间 -- sigmoid
-    
     """
     if mode == "L2":
         # RMSE
@@ -603,7 +518,6 @@ def Loss_func(pos,pred_pos,mode="L2",bool_col = 0):
         return loss_fn(pos,pred_pos)
     
     elif mode == "NL":
-        # 这个loss直接算回归问题有点勉强 -- 后续看怎么处理
         log_ = torch.nn.LogSoftmax(dim=1)
         loss_fn = torch.nn.NLLLoss()
         log_pos = log_(pos)
@@ -617,9 +531,7 @@ def Loss_func(pos,pred_pos,mode="L2",bool_col = 0):
         return dist   
     
 def KL_distance(x,y):
-    # 支持torch_cuda
-    # x,y : []
-    # KL散度支持高维分布计算, logp_y是被指导的,需要取对数
+
     logp_y = F.log_softmax(y, dim = -1)
     p_x = F.softmax(x, dim =- 1)
 
@@ -643,7 +555,6 @@ class AverageMeter():
 
 if __name__ == "__main__":
 
-    # 测试下KL_distance
     x1 = torch.ones((9,12273,3))
     x2 = torch.ones((9,12273,3))
 
